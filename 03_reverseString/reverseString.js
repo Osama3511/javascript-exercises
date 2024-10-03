21b7d1314754ad7bdf99ffb521d7fb13b6b50424
mohamad osama tarakji <hmmosamu@gmail.com>
2024-10-03 22:31:05 +0300

    solved 1 through 7

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reveresedString = '';
+    let length = string.length;
+    for (let i = length - 1 ; i >= 0 ; i--){
+        reveresedString += string[i];
+    }
+    return reveresedString;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fTemp) {
+  let cTemp = ((fTemp - 32) * 5 ) / 9;
+  return Number(cTemp.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+
+
+
+
+const convertToFahrenheit = function(ctemp) {
+  let ftemp = (ctemp * 9) / 5 + 32;
+  return Number(ftemp.toFixed(1));
 };
 
 // Do not edit below this line
